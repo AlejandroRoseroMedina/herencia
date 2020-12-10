54adca85dbc3997ad1fcacf1b3e640f043d3cb88
Alejandro Rosero <ralejandro@unicauca.edu.co>
2020-12-10 10:26:34 -0500

    primer commit herencia

---
+++ src/main/java/com/clearminds/arm/herenciaa/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.arm.herenciaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
