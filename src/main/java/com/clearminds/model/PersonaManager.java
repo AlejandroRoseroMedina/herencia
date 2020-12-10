2e36cd2ba0ab68d622480068f1eb8d43b06a5368
Alejandro Rosero <ralejandro@unicauca.edu.co>
2020-12-10 12:30:18 -0500

    finally

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,30 +1,28 @@
 package com.clearminds.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Properties;
+
 public class PersonaManager {
 	
 	private ServicioPersona serv;
 	
-	public PersonaManager(String nombreClase) throws InstanceException{
+	public PersonaManager() throws InstanceException{
 	//	ServicioPersona servBDD = new ServicioPersonaArchivos();
 	//	serv=servBDD;
+		String nombreClase = leerPropiedad("archivos");
 		Class<?> clase;
 		try {
 			clase = Class.forName(nombreClase);
 			serv=(ServicioPersona)clase.newInstance();
 			
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block	
 			e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de Servicio Persona");
 			
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw new InstanceException("Error al obtener una instancia de Servicio Persona");
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw new InstanceException("Error al obtener una instancia de Servicio Persona");
 		}
 		
 	}
@@ -32,5 +30,25 @@ public class PersonaManager {
 	public void insertarPersona(Persona p){
 		serv.insertar(p);
 	}
+	public static String leerPropiedad(String nombrePropiedad){
+		Properties p = new Properties();
+		File f=new File("config.properties");
+	//	System.out.println("ruta:"+f.getAbsoluteFile());
+		try {
+			p.load(new FileReader("config.properties"));
+			return p.getProperty(nombrePropiedad);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+	
 
 }
--- src/main/java/com/clearminds/model/TestManager.java
+++ src/main/java/com/clearminds/model/TestManager.java
@@ -6,37 +6,18 @@ import java.io.FileReader;
 import java.util.Properties;
 
 public class TestManager {
-	public static String leerPropiedad(String nombrePropiedad){
-		Properties p = new Properties();
-		File f=new File("config.properties");
-	//	System.out.println("ruta:"+f.getAbsoluteFile());
-		try {
-			p.load(new FileReader("config.properties"));
-			return p.getProperty(nombrePropiedad);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-		
-		
-	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String captura = leerPropiedad("archivos"); //bdd para bdd o archivos para la de archivos
-		System.out.println(" es ---- : "+captura);
+	//	String captura = leerPropiedad("archivos"); //bdd para bdd o archivos para la de archivos
+	//	System.out.println(" es ---- : "+captura);
 		
 		
 		try {
 			//PersonaManager p = new PersonaManager("com.clearminds.model.ServicioPersonaBDD");
-			PersonaManager p = new PersonaManager(captura);
+			PersonaManager p = new PersonaManager();
 			System.out.println("pasa");
 			p.insertarPersona(new Persona("ALejandro", "Rosero", 24));
 		} catch (InstanceException e) {
